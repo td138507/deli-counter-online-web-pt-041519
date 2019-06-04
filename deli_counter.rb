53529e7c915dc7aab74fe647239e76e6d0b722b2
Taylor Doyle <tcd56@cornell.edu>
2019-06-04 18:13:28 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+var katz_deli = []
+
+function take_a_number(katz_deli, name)
+katz_deli.push(name)
+return 'Welcome, ${name}. You are number ${katz_deli.length} in line.'
+
+function now_serving(katz_deli)
+  if (katz_deli.length > 0)
+    var now_serving = katz_deli[0]
+    katz_deli.shift()
+    return 'Currently service ${now_serving}.'
+  else
+    return 'There is nobody waiting to be served!'
